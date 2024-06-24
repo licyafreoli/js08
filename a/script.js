9f55691da887dd1f74d68abe6a37bcd3d9b7079b
licyafreoli <168486966+licyafreoli@users.noreply.github.com>
2024-06-24 18:32:16 -0300

    Add files via upload

---
+++ a/script.js
@@ -0,0 +1,13 @@
+
+function getValue() {
+
+    var inputValue = document.getElementById('myInput').value;
+
+    // Exibindo o valor no console
+    // console.log(inputValue);
+
+ let resposta= document.getElementById('output').innerHTML= inputValue;
+    // Colocando o valor no parágrafo com id "output"
+   console.log(resposta)
+}
+    
